2ee4a043421ea3ca15da96520ec91199f90ce028
Alex <103448887+alexgeorgchist@users.noreply.github.com>
2025-03-19 22:19:10 +0300

    Add files via upload

---
+++ database/scripts/scripto.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+SCRIPTNAME=${0##*/}
+
+scriptdir="/storage/emulated/0/t-ui/script"
+logos="/storage/emulated/0/t-ui/ascii"
+line="+++++++++++++++++++++++++++++++++++++++"
+
+## GET SCRIPT LIST
+scriptlist=(6p.sh info.sh pogoda.sh pokoypsaltir.sh sysinfo.sh top_custom_info.sh topinfo.sh mm.sh mmload.sh scriptman.sh) # Massiv tem
+about=(Шестопсалмие Инфопамять Казань О_упокоении О_системе ... ... Выбор_themes Загрузка_mm Загрузка_scripts)
+limit=${#scriptlist[@]} #Number of topics in the array list
+
+menu_script() {
+	printf "%s\n" \
+	           "$line"\
+               "              МОИ СКРИПТЫ:"
+
+var=1 # Number of topics in the array list LIMIT=${#THEME_LIST[@]}
+    while [ "$var" -le "$limit" ] # <=
+    do
+    printf "%-26s%s\n" \
+               " $var. ${scriptlist[$var-1]}" "${about[$var - 1]}"
+var=`expr $var + 1` # Allowed var=$(($var+1)) or let "var += 1"
+    done
+    	printf "%s\n" \
+	              "$line"
+    	
+    exit 0
+}
+
+going () {
+cd $scriptdir
+sh ./$chosen
+exit 0
+}
+
+
+print_header () {
+cat $logos/anonymous.txt
+}
+
+print_end () {
+printf "   %-7s%s\n"\
+          " " "Координаты заменены"\
+          " " "    Restart"
+}
+
+
+## index=$(($1-1))
+## CHOSEN_THEME="{THEME_LIST[$index]}"
+case "$1" in
+      [1-$limit])
+      index=`expr $1 - 1` # Allowed index=$(($1-1))
+  	chosen="${scriptlist[$index]}"
+      going
+      ;;
+     *)
+        printf "%s\n" $line
+        print_header
+        menu_script
+        exit 0
+        ;;
+esac
+
+
+
+
+
